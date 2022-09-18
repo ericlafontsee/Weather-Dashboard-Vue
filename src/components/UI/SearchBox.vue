dbbf47141a6dfa0804d8ae5d0a60f72769f4eca6
Eric LaFontsee <elafontsee@gmail.com>
2022-09-18 13:52:27 -0500

    added api key and fetch for open weather map api

--- src/components/UI/SearchBox.vue
+++ src/components/UI/SearchBox.vue
@@ -7,3 +7,38 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    searchCity() {
+      var APIKey = "205e65f3bbbf65a0b6de249409614a50";
+    //   let city = "Lakeville";
+      var queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${APIKey}`;
+
+      fetch(
+        queryURL
+      ).then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+            console.log("data", data);
+        //   const results = [];
+        //   for (const id in data) {
+        //     results.push({ id: id, password: data[id].password });
+        //   }
+        //   this.results = results;
+        })
+        .catch((error) => {
+          console.log('ERROR:', error);
+        });
+    }
+
+  },
+  mounted() {
+    this.searchCity();
+  },
+};
+</script>
